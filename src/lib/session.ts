3af8402145bfb4fc55b6ee4e30ac4fb3b76a7ebd
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-09-14 20:35:53 -0400

    fixed lib/session to fix vercel error

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -2,18 +2,17 @@
 import { useSession as useNextAuthSession } from "next-auth/react"; // Import the built-in hook
 import { Session } from "next-auth";
 
-interface User {
-  id: string;
-  name?: string;
-  email?: string;
-  image?: string;
-}
-
 interface CustomSession extends Session {
-  user?: User;
+  user: {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
   sessionToken?: string; // Ensure sessionToken is part of the session
 }
 
+// Export a function to use the custom session
 export function useSession() {
   const { data, status } = useNextAuthSession(); // Use the built-in useSession hook
   return { data: data as CustomSession | null, status };
